1e8175f96d645c2fdcd81836022742d15bc2d9b9
hongliang <15711385916@163.com>
2022-11-16 16:46:43 +0800

    Update oh-my-config init.sh

---
+++ oh-my-config/init.sh
@@ -0,0 +1,55 @@
+
+
+# instarll brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+# install oh-my-zsh
+# curl
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# 国内源
+sh -c "$(curl -fsSL https://gitee.com/mirrors/oh-my-zsh/raw/master/tools/install.sh)"
+
+# wget
+# sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# sh -c "$(wget -O- https://gitee.com/pocmon/mirrors/raw/master/tools/install.sh)"
+
+# fetch
+# sh -c "$(fetch -o - https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# install command
+# autojump
+brew install autojump
+# z
+brew install z
+# mtr
+brew install mtr
+# httpie
+brew isntall httpie
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
